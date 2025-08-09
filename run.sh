b36486d4b031d2281ef39c2efe163c794b494c02
Kartik Sarda <149083257+sada-02@users.noreply.github.com>
2025-08-09 19:37:35 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd output
+../src/lexer ../test/test1.txt output1.txt
+../src/lexer ../test/test2.txt output2.txt
+../src/lexer ../test/test3.txt output3.txt
+../src/lexer ../test/test4.txt output4.txt
+../src/lexer ../test/test5.txt output5.txt
+../src/lexer ../test/test6.txt output6.txt
